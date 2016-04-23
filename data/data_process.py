040639a4a6d386e1ce84bdeca715daf66f42b1a6
SJohnson <simonbjohnson@gmail.com>
2016-04-23 17:55:51 +0100

    second commit

--- data/data_process.py
+++ data/data_process.py
@@ -25,6 +25,7 @@ def genAggObj(data,aggList,tree):
     return output
 
 def aggQuestions(qList,aggList,data):
+    checkAggLimits(data,aggList)
     output = {}
     aggIDList = []
     for agg in aggList:
@@ -41,6 +42,18 @@ def aggQuestions(qList,aggList,data):
 
     return output
 
+def checkAggLimits(data,aggList):
+    output = genAggObj(data,aggList,{})
+    aggIDList = []
+    for agg in aggList:
+        aggIDList.append(findCol(agg,data))
+    for d in data[1:]:
+        ref = output
+        for a in aggIDList[:-1]:
+                ref = ref[d[a]]
+        ref[d[aggIDList[len(aggIDList)-1]]]+=1
+    print output
+
 def flattenOutput(data,output,line):
     for key in data:
         if type(data[key]) is dict:
@@ -55,17 +68,23 @@ def flattenOutput(data,output,line):
             
     
 print "Start"
+#krc
 questions = ["What is the main source of drinking water for members of your household?","Main Occupation of the Household Head _ HH","Where do you store water for drinking?","What do you usually do to make the water safer to drink?"]
 aggregatorList = ["Sub-Location"]
 headers = ['Question','Location','Answer','Count'];
 
+#nepal
+#questions =['What is your biggest problem?','Are you satisfied with what NGOs are doing for you after the earthquake?','Are your main problems being addressed?','What is the top thing that you need information about?','Is support provided in a fair way?','Do_you_have_any_health_problem','Occupation']
+#aggregatorList = ['District','Round']
+#headers = ['Question','Location','Round','Answer','Count']
 
 with open('BIDP-Final_Data3.csv', 'rb') as csvfile:
+#with open('nepal.csv', 'rU') as csvfile:
     contents = list(list(rec) for rec in csv.reader(csvfile, delimiter=','))
 
 data = aggQuestions(questions,aggregatorList,contents)
 output = flattenOutput(data,[],[])
-with open('results.csv',"w") as csvfile:
+with open('resultskrc.csv',"w") as csvfile:
     writer = csv.writer(csvfile)
     writer.writerow(headers)
     writer.writerows(output)
--- js/site.js
+++ js/site.js
@@ -1,23 +1,55 @@
+//Nepal
+
+/*var config = {
+	aggregators: ['Round'],
+	color:'#b71c1c',
+	mapcolors:['#cccccc','#FFCDD2','#E57373','#F44336','#B71C1C'],
+	locations:'Location',
+	datafile:'data/resultsnepal.csv',
+	geomfile:'data/nepal_adm3_simplified.geojson',
+	joinAttr:'DISTRICT'
+
+}*/
+
+//krc
 var config = {
-	agggregators: [],
-	color:'#b71c1c'
+	aggregators: [],
+	color:'#b71c1c',
+	mapcolors:['#cccccc','#FFCDD2','#E57373','#F44336','#B71C1C'],
+	locations:'Location',
+	datafile:'data/resultskrc.csv',
+	geomfile:'data/nepal_adm3_simplified.geojson',
+	joinAttr:'DISTRICT'
+
 }
+var map;
+var overlay;
+var mapon = false;
 
 function loadData(){
-	$.ajax(
-		{
-			url: "data/results.csv",
-			success: function(result){
-        		initDash(d3.csv.parse(result));
-    		}
-    	});
+
+	var dataCall = $.ajax({ 
+	    type: 'GET', 
+	    url: config.datafile, 
+	    dataType: 'text',
+	});
+
+	var geomCall = $.ajax({ 
+	    type: 'GET', 
+	    url: config.geomfile, 
+	    dataType: 'json',
+	});
+
+	$.when(dataCall,geomCall).then(function(dataArgs,geomArgs){
+		initDash(d3.csv.parse(dataArgs[0]),geomArgs[0]);
+	});
+
 }
 
-function initDash(data){
+function initDash(data,geom){
+
 	cf = crossfilter(data);
 	cf.questionsDim = cf.dimension(function(d){return d['Question']});
-	//cf.answersDim = cf.dimension(function(d){return d['Answer']});
-	//cf.placesDim = cf.dimension(function(d){return d['Location']});
 
 	cf.questionsGroup = cf.questionsDim.group();
 
@@ -29,7 +61,6 @@ function initDash(data){
 		$('#questions').append('<div id="question'+i+'" class="questionbox">'+q+'</div>');
 		$('#question'+i).on('click',function(){
 			cf.questionsDim.filter(q);
-			$('#graph').html('');
 			$('#question').html(q);
 			genQuestion(cf.questionsDim.top(Infinity));
 		})
@@ -37,15 +68,196 @@ function initDash(data){
 
 	cf.questionsDim.filter(questions[0]);
 	$('#question').html(questions[0]);
+	createMap(geom);
 	genQuestion(cf.questionsDim.top(Infinity));
+	
 }
 
 function genQuestion(data){
 
 	var cf = crossfilter(data);
+	cf.data = data;
+	cf.aggs = [];
+	
 	cf.answersDim = cf.dimension(function(d){return d['Answer']});
-	cf.answersGroup = cf.answersDim.group().reduceSum(function(d){return d['Count']});
+	
+
+	aggregators = [config.locations,'Answer'].concat(config.aggregators);
+
+	aggregators.forEach(function(agg,i){
+		cf.aggs[agg] = {};
+		cf.aggs[agg].dim = cf.dimension(function(d){return d[agg]});
+		cf.aggs[agg].values = cf.aggs[agg].dim.group().all().map(function(v,i){return v.key;});	
+	});
+
+	cf.answersGroup = cf.aggs['Answer'].dim.group().reduceSum(function(d){return d['Count']});
+	cf.locationsGroup = cf.aggs[config.locations].dim.group().reduceSum(function(d){return d['Count']});
+	
+	genDropdowns(cf,[config.locations].concat(config.aggregators));
+	
 	var data = cf.answersGroup.all();
+	var mapData = cf.locationsGroup.all();
+	totalperlocation = {};
+	mapData.forEach(function(d){
+		totalperlocation[d.key] =d.value;
+	});
+	$("input[type=radio][name=chart][value=bar]").prop('checked',true);
+	
+	$('#graph').show();
+	$('#map').hide();
+	/*
+	if($('input[type=radio][name=chart]:checked').val()=='ci'){
+		confidenceGraph(data);
+	} else if($('input[type=radio][name=chart]:checked').val()=='bar'){
+		drawGraph(data,false)
+	} else if($('input[type=radio][name=chart]:checked').val()=='barper'){
+		drawGraph(data,true)
+	}*/
+	drawGraph(data,false);
+	$('#charts').html('<input class="charttype" type="radio" name="chart" value="bar" checked> Bar Chart <input class="charttype" type="radio" name="chart" value="barper"> Bar Chart (percent) <input class="charttype" type="radio" name="chart" value="ci"> 95% confidence intervals <input class="charttype" type="radio" name="chart" value="map"> Map');
+	$('.charttype').on('change',function(){changeRadio(cf);});
+
+	function changeRadio(cf){
+        if($('input[type=radio][name=chart]:checked').val()=='ci'){			
+			$('#graph').show();
+			$('#map').hide();
+			if(mapon){
+				mapon = false;
+				updateDropdowns(cf,config.locations);
+			}			
+			confidenceGraph(data);
+		} else if($('input[type=radio][name=chart]:checked').val()=='bar'){
+			$('#graph').show();
+			$('#map').hide();
+			if(mapon){
+				mapon = false;
+				console.log('bar');
+				console.log(cf);
+				updateDropdowns(cf,config.locations);
+			}			
+			drawGraph(data,false);
+		} else if($('input[type=radio][name=chart]:checked').val()=='barper'){			
+			$('#graph').show();
+			$('#map').hide();
+			if(mapon){
+				mapon = false;
+				updateDropdowns(cf,config.locations);
+			}
+			drawGraph(data,true);			
+		}  else if($('input[type=radio][name=chart]:checked').val()=='map'){
+			$('#graph').hide();
+			$('#map').show();
+			console.log('map');
+			console.log(cf);
+			updateDropdowns(cf,'Answer');
+			mapon = true;
+			updateMap(cf.locationsGroup.all(),cf);
+			map.invalidateSize();
+			map.fitBounds(overlay.getBounds());		
+		} 		
+	}
+}
+
+function genDropdowns(cf,aggs){
+
+	$('#aggregators').html('');
+	aggs.forEach(function(agg,i){
+		createDropdown(cf.aggs[agg].values,cf,i,agg);
+	});
+	
+}
+
+function updateDropdowns(cf,agg){
+	cf.aggs['Answer'].dim.filter();
+	cf.aggs[config.locations].dim.filter();
+	answers = cf.aggs[agg].values;
+	if(agg!="Answer"){
+		answers = ['No filter'].concat(answers);
+	} else {
+		cf.aggs[agg].dim.filter(answers[0]);
+	}
+
+	var html = agg+': <select id="aggchange">';
+
+	answers.forEach(function(a){
+		html = html + '<option value="'+a+'">'+a+'</option> ';
+	});
+
+	html = html + '</option>';
+
+	$('#changeagg').html(html);
+
+	$('#aggchange').on('change',function(){
+		if(this.value=='No filter'){
+			cf.aggs[agg].dim.filter();
+		} else {
+			cf.aggs[agg].dim.filter(this.value);
+		}
+		var data = cf.answersGroup.all();
+		console.log('dropdown');
+		console.log(cf);
+		if($('input[type=radio][name=chart]:checked').val()=='ci'){
+			confidenceGraph(data);		
+		} else if($('input[type=radio][name=chart]:checked').val()=='bar'){
+			drawGraph(data,false);		
+		} else if($('input[type=radio][name=chart]:checked').val()=='barper'){
+			drawGraph(data,true);
+		} else if($('input[type=radio][name=chart]:checked').val()=='map'){
+			updateMap(cf.locationsGroup.all(),cf);
+		}		
+	});		
+}
+
+function createDropdown(answers,cf,i,agg){
+	if(agg!="Answer"){
+		answers = ['No filter'].concat(answers);
+	} else {
+		cf.aggs[agg].dim.filter(answers[0]);
+	}
+	if(agg=="Answer" || agg==config.locations){
+		var html = '<span id="changeagg">'+agg+': <select id="aggchange">';
+		var id = 'change';
+	} else {
+		var html = agg+': <select id="agg'+i+'">';
+		var id = i;
+	}
+
+	answers.forEach(function(a){
+		html = html + '<option value="'+a+'">'+a+'</option> ';
+	});
+
+	html = html + '</option>';
+	if(agg=="Answer" || agg==config.locations){
+		html = html +"</span>"
+	}
+	$('#aggregators').append(html);
+	$('#agg'+id).on('change',function(){
+		if(this.value=='No filter'){
+			cf.aggs[agg].dim.filter();
+		} else {
+			cf.aggs[agg].dim.filter(this.value);
+		}
+		var data = cf.answersGroup.all();
+		if($('input[type=radio][name=chart]:checked').val()=='ci'){
+			confidenceGraph(data);		
+		} else if($('input[type=radio][name=chart]:checked').val()=='bar'){
+			drawGraph(data);		
+		} else if($('input[type=radio][name=chart]:checked').val()=='barper'){
+			drawGraph(data,true);
+		} else if($('input[type=radio][name=chart]:checked').val()=='map'){
+			updateMap(cf.locationsGroup.all(),cf);
+		}		
+	});
+}
+
+function drawGraph(data,percent){
+
+	$('#graph').html('');
+
+	var total=0
+	data.forEach(function(d){
+		total += d.value;
+	});
 
 	var margin = {top: 40, right: 30, bottom: 150, left: 50},
 		width = $("#graph").width() - margin.left - margin.right,
@@ -67,7 +279,7 @@ function genQuestion(data){
 		return d.value;
 	});
 
-	y.domain([0,maxy]);
+	y.domain([0,maxy*1.1]);
 
 	var svg = d3.select("#graph").append("svg")
 		.attr("width", width + margin.left + margin.right)
@@ -78,7 +290,7 @@ function genQuestion(data){
 
     svg.append("g")
 		.attr("class", "x axis baraxis")
-		.attr("transform", "translate(0," + height + ")")
+		.attr("transform", "translate(0," + (height+15) + ")")
 		.call(xAxis)
 		.selectAll("text")  
 		.style("text-anchor", "end")
@@ -107,7 +319,11 @@ function genQuestion(data){
 	    	return y(d.value)+25;	    		
 	    })
 	    .text(function(d){
-	        return d3.format(".3d")(d.value);
+	    	if(percent){
+	    		return d3.format(".1%")(d.value/total);
+	    	} else {
+	    		return d3.format(".3d")(d.value);
+	    	}	        
 	    })
 	    .style("text-anchor", "middle")
 	    .attr("class","numberlabel")
@@ -116,9 +332,218 @@ function genQuestion(data){
 	    	}
 	    	return '#ffffff';	    		
 	    });
+}
+
+function confidenceGraph(data,confidence){
+	var total = 0;
+	confidence = 1.96;
+
+	data.forEach(function(d){
+		total += d.value;
+	});
+
+	data.forEach(function(d){
+		var p = d.value/total;
+		var se = Math.pow((p*(1-p)/total),0.5);
+		ci = d.value/total - confidence*se
+		ci3 = 1-1/(total/3);
+		d.lower = Math.min(ci,ci3);
+		if(d.lower<0){d.lower=0};
+		ci = d.value/total + confidence*se;
+		ci3 = 1/(total/3);
+		d.upper = Math.max(ci,ci3);
+		if(d.upper>1){d.upper=1};
+	});
+	$('#graph').html('');
+
+	var margin = {top: 40, right: 30, bottom: 150, left: 50},
+		width = $("#graph").width() - margin.left - margin.right,
+		height =  450 - margin.top - margin.bottom;
+		
+ 	var x = d3.scale.ordinal()
+        .rangeRoundBands([0, width]);
+
+    var y = d3.scale.linear()
+        .range([height,0]); 
+
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom");
+      
+	x.domain(data.map(function(d) {return d.key; }));
+
+	var maxy = d3.max(data,function(d){
+		return d.value/total;
+	});
+
+	var maxuy = d3.max(data,function(d){
+		return d.upper;
+	});
+
+	y.domain([0,Math.max(maxy*1.1,maxuy)]);	
+
+	var svg = d3.select("#graph").append("svg")
+		.attr("width", width + margin.left + margin.right)
+		.attr("height", height + margin.top + margin.bottom)
+		.append("g")
+		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+    svg.append("g")
+		.attr("class", "x axis baraxis")
+		.attr("transform", "translate(0," + (height+15) + ")")
+		.call(xAxis)
+		.selectAll("text")  
+		.style("text-anchor", "end")
+		 .attr("transform", function(d) {
+		    return "rotate(-35)" 
+		});
+
+	svg.append("g").selectAll("rect")
+	    .data(data)
+	    .enter()
+	    .append("rect") 
+	    .attr("x", function(d,i) { return x(d.key)+3; })
+	    .attr("width", x.rangeBand()-6)
+	    .attr("y", function(d){return y(d.value/total);})
+	    .attr("height", function(d) {return height-y(d.value/total);})
+	    .attr("fill","#eeeeee");					    		    
+
+	svg.append("g").selectAll("line")
+	    .data(data)
+	    .enter()
+	    .append("line") 
+	    .attr("x1", function(d,i) { return x(d.key)+x.rangeBand()*0.35; })
+	    .attr("x2", function(d,i) { return x(d.key)+x.rangeBand()*0.65; })
+	    .attr("y1", function(d){return y(d.upper);})
+	    .attr("y2", function(d) {return y(d.upper);})
+	    .attr("stroke-width",1)
+	    .attr("stroke",config.color);
+
+	svg.append("g").selectAll("line")
+	    .data(data)
+	    .enter()
+	    .append("line") 
+	    .attr("x1", function(d,i) { return x(d.key)+x.rangeBand()*0.35; })
+	    .attr("x2", function(d,i) { return x(d.key)+x.rangeBand()*0.65; })
+	    .attr("y1", function(d){return y(d.lower);})
+	    .attr("y2", function(d) {return y(d.lower);})
+	    .attr("stroke-width",1)
+	    .attr("stroke",config.color);
+
+	svg.append("g").selectAll("line")
+	    .data(data)
+	    .enter()
+	    .append("line") 
+	    .attr("x1", function(d,i) { return x(d.key)+x.rangeBand()/2; })
+	    .attr("x2", function(d,i) { return x(d.key)+x.rangeBand()/2; })
+	    .attr("y1", function(d){return y(d.lower);})
+	    .attr("y2", function(d) {return y(d.upper);})
+	    .attr("stroke-width",1)
+	    .attr("stroke",config.color)
+	    .style("stroke-dasharray", ("3, 3"));	    	    
+
+	svg.append("g").selectAll("line")
+	    .data(data)
+	    .enter()
+	    .append("line") 
+	    .attr("x1", function(d,i) { return x(d.key)+3; })
+	    .attr("x2", function(d,i) { return x(d.key)+x.rangeBand()-3; })
+	    .attr("y1", function(d){return y(d.value/total);})
+	    .attr("y2", function(d) {return y(d.value/total);})
+	    .attr("stroke-width",1)
+	    .attr("stroke",config.color);
+
+	svg.append("g").selectAll("text")
+	    .data(data)
+	    .enter()
+	    .append("text") 
+	    .attr("x", function(d){return x(d.key)+x.rangeBand()/2})
+	    .attr("y", function(d) {return y(d.upper)-10;})
+	    .text(function(d){
+	    	return d3.format(".1%")(d.upper);        
+	    })
+	    .style("text-anchor", "middle")
+	    .attr("class","numberlabel")
+	    .attr("fill",function(d) {return '#000000';});
+
+	svg.append("g").selectAll("text")
+	    .data(data)
+	    .enter()
+	    .append("text") 
+	    .attr("x", function(d){return x(d.key)+x.rangeBand()/2})
+	    .attr("y", function(d) {return y(d.lower)+25;})
+	    .text(function(d){
+	    	return d3.format(".1%")(d.lower);        
+	    })
+	    .style("text-anchor", "middle")
+	    .attr("class","numberlabel")
+	    .attr("fill",function(d) {return '#000000';});	    
+}
+
+function createMap(geom){
+	var base_hotosm = L.tileLayer(
+        'http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png',{
+        attribution: '&copy; OpenStreetMap contributors, <a href="http://hot.openstreetmap.org/">Humanitarian OpenStreetMap Team</a>'}
+    );
+
+	map = L.map('map',{
+				center: [0,0],
+		        zoom: 3,
+		        layers: [base_hotosm]
+			});
+
+	overlay = L.geoJson(geom,{
+	    fillColor: "#eeeeee",
+	    color: "#eeeeee",
+	    weight: 1,
+	    opacity: 0.8,
+	    fillOpacity: 0.6
+	}).addTo(map);
+	
+	$('#map').hide();
 
 }
 
+function updateMap(data,cf){
+	var total = 0;
+	confidence = 1.96;
+	var hash = {};
+
+	data.forEach(function(d){
+		hash[d.key] = d.value;
+	});
+	cf.aggs['Answer'].dim.filter();
+	var mapData = cf.locationsGroup.all();
+	totalperlocation = {};
+	mapData.forEach(function(d){
+		totalperlocation[d.key] =d.value;
+	});
+	cf.aggs['Answer'].dim.filter($('#aggchange').val());
+	overlay.setStyle(style);
+
+	function style(feature){
+		if(feature.properties[config.joinAttr] in hash){
+			var num = hash[feature.properties[config.joinAttr]]/totalperlocation[feature.properties[config.joinAttr]];
+			if(num>0.4){
+				var color = config.mapcolors[4];
+			} else if (num>0.3) {
+				var color = config.mapcolors[3];
+			} else if (num>0.2){
+				var color = config.mapcolors[2];
+			} else{
+				var color = config.mapcolors[1];
+			}
+			
+		} else {
+			var color = config.mapcolors[0];
+		}
+		return {color: color,
+				fillColor:color}
+	}
+
+}
 
 var cf;
+
 loadData();
